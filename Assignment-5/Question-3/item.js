8bc2db42e71173cbab93c12bf244ca9b517ae0b4
Teuton Roy <popeyehk404@gmail.com>
2023-03-24 10:19:18 +0530

    not fixed

---
+++ Assignment-5/Question-3/item.js
@@ -0,0 +1,65 @@
+const items = [
+    {
+      name: "Item 1",
+      image: "./Images/pro1.jpg",
+      price: 100,
+      discount: 10,
+    },
+    {
+      name: "Item 2",
+      image: "/path/to/image2.jpg",
+      price: 50,
+      discount: 0,
+    },
+    {
+      name: "Item 3",
+      image: "/path/to/image3.jpg",
+      price: 200,
+      discount: 20,
+    },
+    {
+        name: "Item 4",
+        image: "/path/to/image4.jpg",
+        price: 150,
+        discount: 15,
+        },
+    {
+        name: "Item 5",
+        image: "/path/to/image5.jpg",
+        price: 250,
+        discount: 25,
+        },
+    {
+        name: "Item 6",
+        image: "/path/to/image6.jpg",
+        price: 300,
+        discount: 30,
+        },
+    {
+        name: "Item 7",
+        image: "/path/to/image7.jpg",
+        price: 350,
+        discount: 35,
+        },
+    {
+        name: "Item 8",
+        image: "/path/to/image8.jpg",
+        price: 400,
+        discount: 40,
+        },
+    {
+        name: "Item 9",
+        image: "/path/to/image9.jpg",
+        price: 450,
+        discount: 45,
+        },
+    {
+        name: "Item 10",
+        image: "/path/to/image10.jpg",
+        price: 500,
+        discount: 50,
+        },
+  ];
+  
+  export default items;
+  
--- Assignment-5/Question-3/script.js
+++ Assignment-5/Question-3/script.js
@@ -1,71 +1,9 @@
+import items from "./items.js";
+
 const itemsPerPage = 5;
 let currentPage = 1;
-const itemListDiv = document.getElementById("itemList");
+const itemListDiv = document.getElementById("item-list");
 const paginationDiv = document.getElementById("pagination");
-
-// Array of items (name, image path, price, and discount)
-const items = [
-  {
-    name: "Item 1",
-    image: "/Image/pro1.jpg",
-    price: 10.00,
-    discount: 2.50
-  },
-  {
-    name: "Item 2",
-    image: "item2.jpg",
-    price: 15.00,
-    discount: null
-  },
-  {
-    name: "Item 3",
-    image: "item3.jpg",
-    price: 20.00,
-    discount: 5.00
-  },
-  {
-    name: "Item 4",
-    image: "item4.jpg",
-    price: 12.00,
-    discount: null
-  },
-  {
-    name: "Item 5",
-    image: "item5.jpg",
-    price: 18.00,
-    discount: 3.00
-  },
-  {
-    name: "Item 6",
-    image: "item6.jpg",
-    price: 25.00,
-    discount: null
-  },
-  {
-    name: "Item 7",
-    image: "item7.jpg",
-    price: 30.00,
-    discount: 7.50
-  },
-  {
-    name: "Item 8",
-    image: "item8.jpg",
-    price: 22.00,
-    discount: null
-  },
-  {
-    name: "Item 9",
-    image: "item9.jpg",
-    price: 27.00,
-    discount: 4.00
-  },
-  {
-    name: "Item 10",
-    image: "item10.jpg",
-    price: 35.00,
-    discount: null
-  }
-];
 
 function displayItems(items) {
   itemListDiv.innerHTML = "";
@@ -101,12 +39,12 @@ function displayPagination(totalItems) {
     if (i === currentPage) {
       pageLink.classList.add("active");
     }
-    pageLink.addEventListener("click", (e) => {
-      e.preventDefault();
-      currentPage = i; // Update the current page
-        displayItems(items); // Display the items for the current page
+    pageLink.addEventListener("click", function () {
+      currentPage = i;
+      displayItems(items);
     });
-
     paginationDiv.appendChild(pageLink);
-    }
+  }
 }
+
+displayItems(items)
