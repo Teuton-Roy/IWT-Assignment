980ce53038d976a21e3ead532ef09d92baa7ea38
Teuton Roy <popeyehk404@gmail.com>
2023-04-04 21:59:54 +0530

    Question 1 and 3

---
+++ Assignment-6/Question-1/script.js
@@ -0,0 +1,28 @@
+const http = require('http');
+const url = require('url');
+
+const server = http.createServer((req, res) => {
+	// parse the URL to get the date of birth
+	const query = url.parse(req.url, true).query;
+	const dob = new Date(query.dob);
+	
+	// calculate the age
+	const today = new Date();
+	const age = today.getFullYear() - dob.getFullYear();
+	if (today < new Date(today.getFullYear(), dob.getMonth(), dob.getDate())) {
+		age--;
+	}
+	
+	// render the age as a webpage
+	res.writeHead(200, {'Content-Type': 'text/html'});
+	res.write('<html><head><title>Your Age</title></head><body>');
+	res.write('<h1>Your Age</h1>');
+	res.write('<p>You were born on ' + dob.toLocaleDateString() + '.</p>');
+	res.write('<p>You are currently ' + age + ' years old.</p>');
+	res.write('</body></html>');
+	res.end();
+});
+
+server.listen(8000, () => {
+	console.log('Server started on port 8000');
+});
---
+++ Assignment-6/Question-3/index.js
@@ -0,0 +1,35 @@
+const express =  require('express')
+
+const app = express()
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+let userIds = []
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+})
+
+app.get('/register', (req, res) => {
+    res.sendFile(__dirname + '/register.html')
+})
+
+app.post('/register', (req, res) => { 
+    const { uname } = req.body;
+    const firstName = uname.split(' ')[0];
+    const existingIds = userIds.filter((id) => id.startsWith(firstName.toLowerCase()+'.'));
+    let highestNumber = 0;
+    existingIds.forEach(id => {
+        const idNumber = parseInt(id.split('.')[1])
+        if (idNumber > highestNumber) {
+            highestNumber = idNumber
+        }
+    });
+    let userId = `${firstName.toLowerCase()}.${highestNumber + 1}`;
+    userIds.push(userId)
+    res.send('Welcome to the club! ' + uname + ' your user id is ' + userId);
+})
+
+app.listen(8080, () => {
+    console.log('Server is running on port 8080')
+})
